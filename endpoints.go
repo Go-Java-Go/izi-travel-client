1d83c9a2a479a92cd5cf0c042eb83b53d24251a2
Arseny Druzhinin <senyasdt4745@gmail.com>
2021-04-16 18:23:08 +0300

    Init client commit

---
+++ endpoints.go
@@ -0,0 +1,16 @@
+package izi_client
+
+const apiKeyHeaderName = "X-IZI-API-KEY"
+
+const apiVersion = "1.8"
+
+const defaultApiHost = "https://api.izi.travel"
+
+const (
+	objectSearchUrl = "/mtg/objects/search"
+	objectByIdUrl   = "/mtgobjects/"
+	objectByIdsUrl  = "/mtgobjects/batch/"
+
+	citiesUrl     = "/cities"
+	citiesByIdUrl = "/cities/"
+)
